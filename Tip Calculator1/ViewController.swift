b9345ac34e7569212f88506839319c5bc585488b
Sudiksha Sarvepalli <ssarvepa@ucsd.edu>
2019-12-20 16:29:26 -0800

    Initial Commit

---
+++ Tip Calculator1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator1
+//
+//  Created by Sudiksha Sarvepalli on 12/20/19.
+//  Copyright © 2019 Sudiksha Sarvepalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
